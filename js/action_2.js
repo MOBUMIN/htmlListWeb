f2b4c7c3adc1d601ff4e10149423c962c6f8aa5f
MOBUMIN <57705512+MOBUMIN@users.noreply.github.com>
2020-10-15 17:41:28 +0900

    mission2 진행중
    대체 왜 style이 없는데?

---
+++ js/action_2.js
@@ -0,0 +1,7 @@
+
+var menu = document.querySelector(".menu-before");
+var nav = document.querySelector(".menu-after");
+function on_menu(){
+    menu.style.display='none';
+    nav.style.display='block';
+}
