6691cf6d66e97805a92a93e26ade1eb48a3635d7
MOBUMIN <57705512+MOBUMIN@users.noreply.github.com>
2020-10-16 22:25:34 +0900

    Slick 사용
    DSC 웹에서는 안되는데 여기서는 된다?

--- js/action_2.js
+++ js/action_2.js
@@ -6,3 +6,9 @@ function off_menu(){
     $('.menu-text').css('display','inline');
     $('.nav').css('display','none');
 }
+$(document).ready(on_menu);
+$(document).ready(off_menu);
+
+$(function(){
+    $(".slider").slick({});
+});
