8532217cd60707ffa745cf2c9dd717472bbbae4b
MOBUMIN <57705512+MOBUMIN@users.noreply.github.com>
2020-10-20 22:00:36 +0900

    jquery, slick 제거

--- js/action_2.js
+++ js/action_2.js
@@ -1,14 +1,39 @@
+/* menu */
+var menutxt=document.getElementsByClassName("menu-text");
+var nav=document.getElementsByClassName("nav");
 function on_menu(){
-    $('.menu-text').css('display','none');
-    $('.nav').css('display','block');
+    menutxt[0].style.display="none";
+    for(var i=0;i<nav.length;i++)
+        nav[i].style.display="block";
 }
 function off_menu(){
-    $('.menu-text').css('display','inline');
-    $('.nav').css('display','none');
+    menutxt[0].style.display="inline";
+    for(var i=0;i<nav.length;i++)
+        nav[i].style.display="none";
 }
-$(document).ready(on_menu);
-$(document).ready(off_menu);
 
-$(function(){
-    $(".slider").slick({});
-});
+/* carousel */
+
+var slideIdx=0;
+var slides = document.getElementsByClassName("slide");
+showSlides(slideIdx);
+
+function getSlideIdx(){
+    for(i=0;i<slides.length;i++){
+        if(slides[i].style.display=="block") return i;
+    }
+}
+
+function plusSlides(n){
+    showSlides(slideIdx+=n);
+}
+
+function showSlides(n){
+    var i;
+    if (n > slides.length-1) {slideIdx = 0}
+    if(n < 0) {slideIdx=slides.length-1}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIdx].style.display = "block";
+}
